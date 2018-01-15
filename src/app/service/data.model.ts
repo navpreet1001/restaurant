eff321fd74bfb5e26e352b185b5094ab0b1b1fa8
Navpreet <navpreetgagar@gmail.com>
2018-01-15 16:01:38 -0400

    playing with Service

--- src/app/service/data.model.ts
+++ src/app/service/data.model.ts
@@ -1,4 +1,28 @@
 export class Data {
+    // Broken Model
+    // owner :{
+    //   name: string,
+    //   phone: number,
+    //   address: {
+    //     street:string,
+    //     city: string,
+    //     province: string
+    //   },
+    //   hours: [{
+    //       day: string,
+    //       hour: string
+    //     }]
+    // }
+    // menu: [{
+    //   title: string,
+    //   id: number,
+    //   item: [{
+    //     name: string,
+    //     desc: string,
+    //     price: number
+    //     }]
+    // }];
+
 
  // constructor (name: string, amount:number){
  //   this.name = name;
