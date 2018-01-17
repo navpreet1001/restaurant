f559a2c7d922a35827c0da404c403d1e20284ccf
Navpreet <navpreetgagar@gmail.com>
2018-01-17 00:11:37 -0400

    Service Updated and Menu Style Updated

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,57 +10,7 @@ import { DataService } from '../service/data.service';
 export class MenuComponent implements OnInit {
 
   data = {};
-// Working from here
 
-  // public menu = {
-	// "food": [{
-	// 		"title": "Appetizer",
-	// 		"id": "01",
-	// 		"item": [{
-  //       "name": "Paneer Tikka",
-  //       "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
-  //       "price": 152
-	// 			},
-	// 			{
-  //         "name": "Samosa",
-  //       	"desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
-  //       	"price": 110
-	// 			},{
-  //         "name": "Chicken Tikka",
-  //       	"desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
-  //       	"price": 140
-	// 			},{
-  //         "name": "Chicken Wings",
-  //       	"desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
-  //       	"price": 150
-	// 			}
-	// 		]
-	// 	},
-	// 	{
-	// 		"title": "Entree",
-	// 		"id": "02",
-	// 		"item": [{
-  //         "name": "Pizza",
-  //         "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
-  //         "price": 130
-	// 			},{
-  //         "name": "Pasta",
-  //       	"desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
-  //       	"price": 140
-	// 			},{
-  //         "name": "Butter Chicken",
-  //       	"desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
-  //       	"price": 170
-	// 			},{
-  //         "name": "Sahi Paneer",
-  //       	"desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
-  //       	"price": 160
-	// 			}
-	// 		]
-	// 	}
-  //
-	// ]
-  // };
   constructor(private dataService :DataService) { }
 
   ngOnInit() {
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -19,9 +19,8 @@ export class DataService {
   			}, {
   				"day": "Closed",
   				"hour": "Closed"
-  			}
-  		]
-  	},
+  			}]
+      },
     "menu": [{
 			"title": "Appetizer",
 			"id": "01",
@@ -42,10 +41,8 @@ export class DataService {
           "name": "Chicken Wings",
         	"desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
         	"price": 150
-				}
-			]
-		},
-		{
+				}]
+		},{
 			"title": "Entree",
 			"id": "02",
 			"item": [{
@@ -65,7 +62,55 @@ export class DataService {
         	"desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A temporibus dolorum animi.",
         	"price": 160
 				}]
-		}]
+		},{
+			"title": "Desserts",
+			"id": "03",
+			"item": [{
+          "name": "Gulab Jamun",
+          "desc": "Round milk dumplings fried and immersed in a sweet cardamom rose water.",
+          "price": 90
+				},{
+          "name": "Kheer",
+        	"desc": "A favourite silky rice pudding served with an infusion of rose syrup and sprinkled with raisin and nuts.",
+        	"price": 95
+				},{
+          "name": "Kulfi",
+        	"desc": "An Indian variety of gelato available in a variety of flavours.",
+        	"price": 75
+				},{
+          "name": "Gajar Halwa",
+        	"desc": "Grated carrots cooked gently in milk and sprinkled generously with nuts.",
+        	"price": 80
+				},{
+          "name": "Rasmlai",
+        	"desc": "Delicate patties of shaped cheese soaked in a thick milk flavoured with cardamom and saffron.",
+        	"price": 100
+				}]
+      }],
+      "drinks":{
+        "title": "Beverages",
+        "item": [{
+            "name": "Mango Lassi",
+            "desc": "Cold sweetened mango and yogurt drink",
+            "price": 90
+          },{
+            "name": "Sweet Lassi",
+            "desc": "Cold sweetened yogurt drink",
+            "price": 95
+          },{
+            "name": "Tea",
+            "desc": "Chai Tea, Spiced Tea, Iced Tea ",
+            "price": 75
+          },{
+            "name": "Coffe",
+            "desc": "Black coffe or with milk",
+            "price": 80
+          },{
+            "name": "Soda Pop",
+            "desc": "Coke, Diet Coke, Sprite, Lemonade, Fanta",
+            "price": 100
+          }]
+      }
   };
 
   getData(){
